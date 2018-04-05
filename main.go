1af03e302f7d1c394b7ba3a6f363dd81f0f41835
fred <fred.battista@qntfy.com>
2018-04-05 14:57:29 +0200

    Playing around with manual listing of domains to ignore; settling on one-collector per domain approach

--- main.go
+++ main.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
 	// Instantiate default collector
+	//disallowedDomainOne, _ := regexp.Compile("twitter.com")
+	//disallowedDomainTwo, _ := regexp.Compile("google.com")
+	//disallowedDomainThree, _ := regexp.Compile("facebook.com")
+	disallowedDomains := []string{
+		"twitter.com", "google.com",
+		"google.es", "facebook.com",
+		"nakedcapitalism.com/author", "apple.com",
+		"mozilla.org", "wsj.com", "youtube.com", "pixels.com", "shopify.com",
+		"pinterest.com", "footbie.com", "linkedin.com", ".wikimedia.", "wikipedia.", "digg.com", "myspace.com"}
+
 	c := colly.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("nakedc.org", "wiki.hackerspaces.org"),
-		c
-	
+		colly.MaxDepth(1),
+		colly.AllowedDomains("deadspin.com"),
+		//colly.Async(true),
 	)
+
+	//c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 2})
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
+		uri := e.Request.AbsoluteURL(link)
 		// Print link
 		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 		// Visit link found on page
 		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		shouldI := true
+		for _, d := range disallowedDomains {
+			if strings.Contains(uri, d) {
+				shouldI = false
+			}
+		}
+		if shouldI {
+			c.Visit(uri)
+		}
 	})
 
 	// Before making a request print "Visiting ..."
@@ -30,6 +52,7 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	//c.Visit("https://nakedcapitalism.com/")
+	c.Visit("https://deadspin.com")
+	//c.Wait()
 }
